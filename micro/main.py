3ef963ccf5a3f5c266cf34f2e203262c1ece67ba
Lucas <lucas-camargo@outlook.com>
2022-01-06 00:13:13 +0000

    changes

--- micro/main.py
+++ micro/main.py
@@ -1,2 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route ('/')
+def app(event):
+    return "Olá, mundo!"
+
+@app.route ('/login')
 def app(event):
     return "Hello, world!"
