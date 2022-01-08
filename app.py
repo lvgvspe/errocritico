f4e0104272a427e08c70100d29b079e8392134ff
Lucas <lucas-camargo@outlook.com>
2022-01-08 21:33:44 +0000

    commit

--- app.py
+++ app.py
@@ -3,17 +3,20 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return 'index'
 
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return f'Subpath {escape(subpath)}'
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
 
 with app.test_request_context():
-    print(url_for('indice'))
+    print(url_for('index'))
     print(url_for('login'))
     print(url_for('login', next='/'))
     print(url_for('profile', username='John Doe'))
