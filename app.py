1568331e4ab12e16bd947df06c1ad67f3efca482
Lucas <lucas-camargo@outlook.com>
2022-01-08 21:21:30 +0000

    commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -11,3 +11,14 @@ def hello_world():
 def show_subpath(subpath):
     # show the subpath after /path/
     return f'Subpath {escape(subpath)}'
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
+
+
+
+if __name__='__main__':
+    app.run(debug=True)
