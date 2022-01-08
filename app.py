c1a602f4cfbf5a161da9e2b0d3a548dd7d84e1a0
Lucas <lucas-camargo@outlook.com>
2022-01-08 20:56:31 +0000

    commit

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
