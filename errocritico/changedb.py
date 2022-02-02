e0d391dc3c38120b773c60e552d0616e52931a91
Lucas <lucas-camargo@outlook.com>
2022-02-02 15:06:24 -0300

    Create changedb.py

---
+++ errocritico/changedb.py
@@ -0,0 +1,12 @@
+import os
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+cur = conn.cursor()
+
+cur.execute = "DROP TABLE IF EXISTS test;"
+
+conn.commit()
