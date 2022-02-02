6035cb61959764ac3dfe9922aff2e0fec885abfb
Lucas <lucas-camargo@outlook.com>
2022-02-01 22:17:44 -0300

    Update blog.py

--- errocritico/blog.py
+++ errocritico/blog.py
@@ -17,7 +17,7 @@ def index():
     db = get_db()
     posts = db.execute(
         'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' FROM posts p JOIN users u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
@@ -38,7 +38,7 @@ def create():
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id)'
+                'INSERT INTO posts (title, body, author_id)'
                 ' VALUES (?, ?, ?)',
                 (title, body, g.user['id'])
             )
@@ -50,7 +50,7 @@ def create():
 def get_post(id, check_author=True):
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' FROM posts p JOIN users u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
     ).fetchone()
@@ -81,7 +81,7 @@ def update(id):
         else:
             db = get_db()
             db.execute(
-                'UPDATE post SET title = ?, body = ?'
+                'UPDATE posts SET title = ?, body = ?'
                 ' WHERE id = ?',
                 (title, body, id)
             )
@@ -95,7 +95,7 @@ def update(id):
 def delete(id):
     get_post(id)
     db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.execute('DELETE FROM posts WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('blog.index'))
 
@@ -105,11 +105,11 @@ def profile(username):
     db = get_db()
     user = db.execute(
         'SELECT id, username, password, email, name, surname, location, country, state, zipcode, aboutme, birth, gender, private_profile, private_email, private_zipcode, private_birth, private_gender'
-        ' FROM user WHERE username = ?', (username,)
+        ' FROM users WHERE username = ?', (username,)
     ).fetchall()
     posts = db.execute(
         'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' FROM posts p JOIN users u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
     for u in user:
@@ -122,7 +122,7 @@ def map():
     db = get_db()
     users = db.execute(
         'SELECT id, username, password, email, name, surname, location, country, state, zipcode, aboutme, birth, gender, private_profile, private_email, private_zipcode, private_birth, private_gender'
-        ' FROM user'
+        ' FROM users'
     ).fetchall()
 
     local = []
