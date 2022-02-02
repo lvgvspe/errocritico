ca5dd1296c567167ab7b1893a0d95f5a78376097
Lucas <lucas-camargo@outlook.com>
2022-02-01 22:30:50 -0300

    Update blog.py

--- errocritico/blog.py
+++ errocritico/blog.py
@@ -15,11 +15,12 @@ bp = Blueprint('blog', __name__)
 @bp.route('/')
 def index():
     db = get_db()
-    posts = db.cursor().execute(
+    db.cursor().execute(
         'SELECT p.id, title, body, created, author_id, username'
         ' FROM posts p JOIN users u ON p.author_id = u.id'
         ' ORDER BY created DESC'
-    ).fetchall()
+    )
+    posts = db.cursor().fetchall()
     return render_template('blog/index.html', posts=posts)
 
 @bp.route('/create', methods=('GET', 'POST'))
