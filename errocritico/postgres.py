3c651c74265d423f507c5bd77777926052f259ac
Lucas <lucas-camargo@outlook.com>
2022-02-01 21:55:22 -0300

    Create postgres.py

---
+++ errocritico/postgres.py
@@ -0,0 +1,9 @@
+import os
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+db = psycopg2.connect(DATABASE_URL, sslmode='require')
+cursor = db.cursor()
+with open('schema.sql') as f:
+    cursor.execute(f.read().decode('utf8'))
