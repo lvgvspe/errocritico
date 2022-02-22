4b71c88cdb7e50942eb68535cd0043c142c9410c
Lucas <lucas-camargo@outlook.com>
2022-02-22 03:36:11 -0300

    fixed factory blueprint

--- errocritico/__init__.py
+++ errocritico/__init__.py
@@ -35,7 +35,4 @@ def create_app(test_config=None):
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
 
-    from . import uploader
-    app.register_blueprint(uploader.bp)
-
     return app
